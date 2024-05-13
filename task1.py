692a98e617072dec3e5adae6a575aa7e85cd7779
Makson <maksim.saburov228@gmail.com>
2024-05-13 22:41:28 +0300

    first

---
+++ task1.py
@@ -0,0 +1,25 @@
+DATABASE = {
+    'Серёга': 'Омск',
+    'Соня': 'Москва',
+    'Миша': 'Москва',
+    'Дима': 'Челябинск',
+    'Алина': 'Красноярск',
+    'Егор': 'Пермь',
+    'Коля': 'Красноярск'
+}
+
+def process_anfisa(query):
+    if query == 'Сколько у меня друзей?':
+        count = len(DATABASE)
+        return 'У тебя ' + str(count) + ' друзей.'
+    elif query == 'Кто все мои друзья?':
+        friends_string = ''
+        for i in DATABASE:
+            friends_string = str(friends_string) + str(i) + ' '
+        return 'Твои друзья: ' + str(friends_string)
+    else:
+        return '<неизвестный запрос>'
+
+print('Привет, я Анфиса!')
+print(process_anfisa('Сколько у меня друзей?'))
+print(process_anfisa('Кто все мои друзья?'))
---
+++ task3.py
@@ -0,0 +1,33 @@
+DATABASE = {
+    'Серёга': 'Омск',
+    'Соня': 'Москва',
+    'Миша': 'Москва',
+    'Дима': 'Челябинск',
+    'Алина': 'Красноярск',
+    'Егор': 'Пермь',
+    'Коля': 'Красноярск'
+}
+
+def process_anfisa(query):
+    if query == 'Сколько у меня друзей?':
+        count = len(DATABASE)
+        return 'У тебя ' + str(count) + ' друзей.'
+    elif query == 'Кто все мои друзья?':
+        friends_string = ''
+        for i in DATABASE:     
+            friends_string += i + ' '
+        a = str('Твои друзья: ' + friends_string)
+        return a
+    
+    elif query == 'Где все мои друзья?':
+        unique_cities = set(DATABASE.values())
+        friends_city = ' '.join(unique_cities)
+        return 'Твои друзья в городах: ' + friends_city
+    else:
+        return '<неизвестный запрос>'
+
+# Не изменяйте следующий код
+print('Привет, я Анфиса!')
+print(process_anfisa('Сколько у меня друзей?'))
+print(process_anfisa('Кто все мои друзья?'))
+print(process_anfisa('Где все мои друзья?'))
